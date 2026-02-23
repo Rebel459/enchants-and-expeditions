41c500a62d26185783ee02e99a453f62bebc91a1
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-02-23 18:11:47 +1100

    v2.4.8

--- src/main/java/net/legacy/enchants_and_expeditions/mixin/inventory/GrindstoneMenuMixin.java
+++ src/main/java/net/legacy/enchants_and_expeditions/mixin/inventory/GrindstoneMenuMixin.java
@@ -1,8 +1,11 @@
 package net.legacy.enchants_and_expeditions.mixin.inventory;
 
 import net.legacy.enchants_and_expeditions.tag.EaEEnchantmentTags;
+import net.legacy.enchants_and_expeditions.util.EnchantingHelper;
+import net.minecraft.core.Holder;
 import net.minecraft.tags.EnchantmentTags;
 import net.minecraft.world.inventory.GrindstoneMenu;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.ItemEnchantments;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,8 +24,22 @@ public abstract class GrindstoneMenuMixin {
             ),
             index = 1
     )
-    private Consumer<ItemEnchantments.Mutable> EaE$keepBlessings(Consumer<ItemEnchantments.Mutable> updater) {
-        return mutable -> mutable.removeIf(holder ->
-                !holder.is(EnchantmentTags.CURSE) && !holder.is(EaEEnchantmentTags.BLESSING));
+    private Consumer<ItemEnchantments.Mutable> EaE$grindstoneBlessingsAndCurses(Consumer<ItemEnchantments.Mutable> updater) {
+        return mutable -> {
+            var enchantList = mutable.keySet().stream().toList();
+            boolean hasEnchants = false;
+            for (Holder<Enchantment> enchant : enchantList) {
+                if (EnchantingHelper.isEnchantment(enchant)) {
+                    hasEnchants = true;
+                    break;
+                }
+            }
+            if (hasEnchants) {
+                mutable.removeIf(holder -> !holder.is(EnchantmentTags.CURSE) && !holder.is(EaEEnchantmentTags.BLESSING));
+            }
+            else {
+                mutable.removeIf(holder -> true);
+            }
+        };
     }
 }
