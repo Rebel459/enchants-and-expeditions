3dbdf801b3b5ea066d0f2fe3ee09a5caa96961c6
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-07-26 14:17:00 +1000

    26.1-r1.5

--- common/src/main/java/net/rebel459/enchants_and_expeditions/mixin/entity/PlayerMixin.java
+++ common/src/main/java/net/rebel459/enchants_and_expeditions/mixin/entity/PlayerMixin.java
@@ -53,60 +53,10 @@ public abstract class PlayerMixin implements FeatherFlightInterface {
     protected void EaE$experienceRebalance(CallbackInfoReturnable<Integer> cir) {
         if (!EaEConfig.get().general.experience_rebalance) return;
 
-        // 0-30
-
-        if (this.experienceLevel <= 5)
-            cir.setReturnValue(20);
-        else if (this.experienceLevel <= 10)
-            cir.setReturnValue(30);
-        else if (this.experienceLevel <= 15)
-            cir.setReturnValue(40);
-        else if (this.experienceLevel <= 20)
-            cir.setReturnValue(55);
-        else if (this.experienceLevel <= 25)
-            cir.setReturnValue(70);
-        else if (this.experienceLevel <= 30)
-            cir.setReturnValue(85);
-
-        // 31-90
-
-        else if (this.experienceLevel <= 40)
-            cir.setReturnValue(100);
-        else if (this.experienceLevel <= 50)
-            cir.setReturnValue(110);
-        else if (this.experienceLevel <= 60)
-            cir.setReturnValue(120);
-        else if (this.experienceLevel <= 70)
-            cir.setReturnValue(130);
-        else if (this.experienceLevel <= 80)
-            cir.setReturnValue(140);
-        else if (this.experienceLevel <= 90)
-            cir.setReturnValue(150);
-
-        // 91+
-
-        else if (this.experienceLevel == 91)
-            cir.setReturnValue(200);
-        else if (this.experienceLevel == 92)
-            cir.setReturnValue(250);
-        else if (this.experienceLevel == 93)
-            cir.setReturnValue(300);
-        else if (this.experienceLevel == 94)
-            cir.setReturnValue(350);
-        else if (this.experienceLevel == 95)
-            cir.setReturnValue(400);
-        else if (this.experienceLevel == 96)
-            cir.setReturnValue(500);
-        else if (this.experienceLevel == 97)
-            cir.setReturnValue(600);
-        else if (this.experienceLevel == 98)
-            cir.setReturnValue(800);
-        else if (this.experienceLevel == 99)
-            cir.setReturnValue(1100);
-        else if (this.experienceLevel == 100)
-            cir.setReturnValue(1500);
+        if (this.experienceLevel < 100)
+            cir.setReturnValue(20 + this.experienceLevel * 2);
         else
-            cir.setReturnValue(5000);
+            cir.setReturnValue(500 + (this.experienceLevel - 99) * 50);
     }
 
     @Inject(at = @At("TAIL"), cancellable = true, method = "getProjectile(Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/world/item/ItemStack;")
@@ -162,7 +112,7 @@ public abstract class PlayerMixin implements FeatherFlightInterface {
         int level = EnchantingHelper.getLevel(stack, EaEEnchantments.CRITICAL_STRIKE);
         if (level > 0) {
             int random = player.getRandom().nextIntBetweenInclusive(1, 10);
-            if (level >= random) canCriticalAttack  = true;
+            if (level + 1 >= random) canCriticalAttack  = true;
         }
         return canCriticalAttack;
     }
